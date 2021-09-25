34f61167894db5d392cb23d4cf4792c5ca3198a4
Abay129 <satybekov.129@bk.ru>
2021-09-25 15:50:35 +0300

    всё остальное

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,23 +1,68 @@
 package com.company;
 
+import com.sun.source.tree.IfTree;
+
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Animal {
-    String name;
-    Integer age;
-    Integer sitost;
-    Integer moral;
-    Integer health;
-    Integer mid;
-    Random r = new Random();
+     String name;
+     Integer age;
+     Integer sitost;
+     Integer moral;
+     Integer health;
+     Integer mid;
+     boolean doSMTHG;
+    private static final Random r = new Random();
 
-    public Animal(String name) {
+
+    public  Animal(String name) {
         this.name = name;
         this.age = r.nextInt(18);
         this.sitost = r.nextInt(100);
         this.moral = r.nextInt(100);
         this.health = r.nextInt(100);
         this.mid = (sitost+moral+health)/3;
+        this.doSMTHG = false;
+    }
+    public static Animal makeCat(){
+        Scanner s = new Scanner(System.in);
+        System.out.println("введите имя кота");
+        String name = s.nextLine();
+        System.out.println("введите возраст кота");
+        Integer age = s.nextInt();
+        Animal cat = new Animal(name);
+        cat.setAge(age);
+        return cat;
+    }
+
+    public static void nextDayChange(Animal animal){
+        Integer shok = r.nextInt(2);
+        Integer hel =  r.nextInt(3);
+
+        Integer mor = r.nextInt(3);
+        if (shok==0){
+            animal.moral = animal.moral + mor;
+        }else {
+            animal.moral = animal.moral - mor;
+        }
+        Integer shok2 = r.nextInt(2);
+        if (shok2==0){
+            animal.health = animal.health + hel;
+        }else {
+            animal.health = animal.health - hel;
+        }
+        Integer sit = r.nextInt(5);
+
+        animal.sitost = animal.sitost - sit;
+    }
+    public Integer getMid() {
+        return mid;
+    }
+
+    public void setMid(Integer mid) {
+        this.mid = mid;
     }
 
     public String getName() {
@@ -56,7 +101,70 @@ public class Animal {
         return health;
     }
 
+    public  void changeHealthForFive(){
+         this.age = age + 7;
+         this.moral = moral - 3;
+         this.sitost = sitost - 3;
+    }
+    public  void changeHealthForTeen(){
+        this.age = age + 5;
+        this.moral = moral - 5;
+        this.sitost = sitost - 5;
+    }
+    public  void changeHealthForOld(){
+        this.age = age + 4;
+        this.moral = moral - 6;
+        this.sitost = sitost - 6;
+    }
+    public  void changeSitostForFive(){
+        this.sitost = sitost + 7;
+    }
+    public  void changeSitostForTeen(){
+
+        this.sitost = sitost + 5;
+    }
+    public  void changeSitostForOld(){
+
+        this.sitost = sitost + 4;
+    }
+    public  void changeMoralForFive(){
+        this.age = age + 7;
+        this.moral = moral + 7;
+        this.sitost = sitost - 3;
+    }
+    public  void changeMoralForTeen(){
+
+        this.age = age + 5;
+        this.moral = moral + 5;
+        this.sitost = sitost - 5;
+    }
+    public  void changeMoralForOld(){
+        this.age = age + 4;
+        this.moral = moral + 4;
+        this.sitost = sitost - 6;
+    }
+
     public void setHealth(Integer health) {
         this.health = health;
+    }
+
+    public static final void toPrint(Animal animal) {
+        System.out.println("-----+----------+----------+----------+----------+----------+---------------+");
+        String f ="%-10s|%-10s|%-10s|%-10s|%-10s|%-15s|";
+        System.out.println(String.format(f, animal.getName(), animal.getAge(), animal.getHealth(), animal.getMoral(), animal.getSitost(), animal.getMid()));
+    }
+    private static final void printHeader() {
+        System.out.println(String.format("|%1$10.10s | %1$-10.10s | %1$-10.10s | %1$-10.10s | %1$-10.10s | %1$-10.10s |", "--------------"));
+        System.out.println(String.format("|%10.10s | %-10.10s | %-10.10s | %-10.10s | %-10.10s | %-10.10s |", "имя", "возраст", "здоровье", "настроение", "сытость", "средний уровень"));
+        System.out.println(String.format("|%1$10.10s | %1$-10.10s | %1$-10.10s | %1$-10.10s | %1$-10.10s | %1$-10.10s |", "--------------"));
+    }
+
+    public static void printCats(List<Animal> animals) {
+        printHeader();
+        animals.forEach(Animal::toPrint);
+        System.out.println();
+    }
+    public static int byMid(Animal c1, Animal c2) {
+        return c2.mid.compareTo(c1.mid);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,178 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
-
+    static Scanner  s  = new Scanner(System.in);
     public static void main(String[] args) {
+        ArrayList<Animal> animals = new ArrayList<>();
+        Animal cat1 = new Animal("Peach");
+        Animal cat2 = new Animal("Jasper");
+        Animal cat3 = new Animal("Poppy");
+        animals.add(cat1);
+        animals.add(cat2);
+        animals.add(cat3);
+        animals.sort(Animal::byMid);
+        Animal.printCats(animals);
+        while (true) {
+            System.out.println("что делать");
+            System.out.println("1: покормить, 2: поиграть, 3: сводить к ветеринару, 4: завести ещё питомца, 5:следующий день");
+            Integer i = s.nextInt();
+            if (i == 4) {
+                animals.add(Animal.makeCat());
+                animals.sort(Animal::byMid);
+            } else if (i == 3) {
+                Animal.printCats(animals);
+                System.out.println("С каким котом ");
+                Integer p = s.nextInt();
+                if (i == 1) {
+                    if (animals.get(1).age <= 5) {
+                        animals.get(1).changeHealthForFive();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы сводили к врачу %s", animals.get(1).name));
+                    } else if (animals.get(1).age <= 10 && animals.get(1).age >= 6) {
+                        animals.get(1).changeHealthForTeen();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы сводили к врачу %s", animals.get(1).name));
+                    } else if (animals.get(1).age >= 11) {
+                        animals.get(1).changeHealthForOld();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы сводили к врачу %s", animals.get(1).name));
+                    }
+                } else if (i == 2) {
+                    if (animals.get(2).age <= 5) {
+                        animals.get(2).changeHealthForFive();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы сводили к врачу %s", animals.get(2).name));
+                    } else if (animals.get(2).age <= 10 && animals.get(2).age >= 6) {
+                        animals.get(2).changeHealthForTeen();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы сводили к врачу %s", animals.get(2).name));
+                    } else if (animals.get(2).age >= 11) {
+                        animals.get(2).changeHealthForOld();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы сводили к врачу %s", animals.get(2).name));
+                    }
+                } else if (i == 3) {
+                    if (animals.get(3).age <= 5) {
+                        animals.get(3).changeHealthForFive();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы сводили к врачу %s", animals.get(3).name));
+                    } else if (animals.get(3).age <= 10 && animals.get(3).age >= 6) {
+                        animals.get(3).changeHealthForTeen();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы сводили к врачу %s", animals.get(3).name));
+                    } else if (animals.get(3).age >= 11) {
+                        animals.get(3).changeHealthForOld();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы сводили к врачу %s", animals.get(3).name));
+                    }
+                }
+
+            }else if (i == 2){
+                Animal.printCats(animals);
+                System.out.println("С каким котом ");
+                Integer p = s.nextInt();
+                if (i == 1) {
+                    if (animals.get(1).age <= 5) {
+                        animals.get(1).changeMoralForFive();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы поиграли с %s", animals.get(1).name));
+                    } else if (animals.get(1).age <= 10 && animals.get(1).age >= 6) {
+                        animals.get(1).changeMoralForTeen();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы поиграли с %s", animals.get(1).name));
+                    } else if (animals.get(1).age >= 11) {
+                        animals.get(1).changeMoralForOld();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы поиграли с %s", animals.get(1).name));
+                    }
+                } else if (i == 2) {
+                    if (animals.get(2).age <= 5) {
+                        animals.get(2).changeMoralForFive();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы поиграли с %s", animals.get(2).name));
+                    } else if (animals.get(2).age <= 10 && animals.get(2).age >= 6) {
+                        animals.get(2).changeMoralForTeen();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы поиграли с %s", animals.get(2).name));
+                    } else if (animals.get(2).age >= 11) {
+                        animals.get(2).changeMoralForOld();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы поиграли с %s", animals.get(2).name));
+                    }
+                } else if (i == 3) {
+                    if (animals.get(3).age <= 5) {
+                        animals.get(3).changeMoralForFive();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы поиграли с %s", animals.get(3).name));
+                    } else if (animals.get(3).age <= 10 && animals.get(3).age >= 6) {
+                        animals.get(3).changeMoralForTeen();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы поиграли с %s", animals.get(3).name));
+                    } else if (animals.get(3).age >= 11) {
+                        animals.get(3).changeMoralForOld();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы поиграли с %s", animals.get(3).name));
+                    }
+                }
+
+            }else if(i == 1){
+                Animal.printCats(animals);
+                System.out.println("С каким котом ");
+                Integer p = s.nextInt();
+                if (i == 1) {
+                    if (animals.get(1).age <= 5) {
+                        animals.get(1).changeSitostForFive();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы покормили %s", animals.get(1).name));
+                    } else if (animals.get(1).age <= 10 && animals.get(1).age >= 6) {
+                        animals.get(1).changeSitostForTeen();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы покормили %s", animals.get(1).name));
+                    } else if (animals.get(1).age >= 11) {
+                        animals.get(1).changeSitostForOld();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы покормили %s", animals.get(1).name));
+                    }
+                } else if (i == 2) {
+                    if (animals.get(2).age <= 5) {
+                        animals.get(2).changeSitostForFive();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы покормили %s", animals.get(2).name));
+                    } else if (animals.get(2).age <= 10 && animals.get(2).age >= 6) {
+                        animals.get(2).changeSitostForTeen();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы покормили %s", animals.get(2).name));
+                    } else if (animals.get(2).age >= 11) {
+                        animals.get(2).changeSitostForOld();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы покормили %s", animals.get(2).name));
+                    }
+                } else if (i == 3) {
+                    if (animals.get(3).age <= 5) {
+                        animals.get(3).changeSitostForFive();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы покормили %s", animals.get(3).name));
+                    } else if (animals.get(3).age <= 10 && animals.get(3).age >= 6) {
+                        animals.get(3).changeSitostForTeen();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы покормили %s", animals.get(3).name));
+                    } else if (animals.get(3).age >= 11) {
+                        animals.get(3).changeSitostForOld();
+                        animals.sort(Animal::byMid);
+                        System.out.println(String.format("Вы покормили %s", animals.get(3).name));
+                    }
+                }
+
+            }else if(i == 5){
+                animals.forEach(Animal::nextDayChange);
+                Animal.printCats(animals);
+            }
 
 
+        }
     }
 }
